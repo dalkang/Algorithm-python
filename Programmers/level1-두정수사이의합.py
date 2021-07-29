45ec931d370c37b8977cda8f530bde64560c15c6
cubodado <dalkang000@gmail.com>
2021-07-29 23:15:23 +0900

    210729

---
+++ Programmers/level1-두정수사이의합.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    answer = 0
+    start = a
+    end = b
+    
+    if a > b:
+        start = b
+        end = a
+    for i in range(start, end + 1):
+        answer += i
+        
+    return answer
